906721a380b7b016a0d8d6c5bc736533053548ea
Matheus Mendes <bigodines@gmail.com>
2012-03-13 22:37:25 -0300

    hello world with a very tiny logic

--- sample_app/frontend.js
+++ sample_app/frontend.js
@@ -4,17 +4,31 @@ it requires ExpressJS to run. (http://expressjs.com)
 */
 
 var express = require('express');
-var app = express.createServer();
+var app = express.createServer(
+    express.logger(),
+    express.bodyParser()
+    
+);
 var feature_flipper = require('../feature_flipper');
 
 app.configure(function() {
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(app.router);
 });
 
 
-app.get('/:user?', function(req, res) {
-    res.send('hello '+ req.params.user);
+app.all('/:user?', function(req, res, next) {
+    var user = req.params.user;
+    if (user) {
+	res.send('hello '+ user);
+    } else {
+	next();
+    }
 
+});
+
+app.get('/', function( req, res) {
+    res.send('this is a sample application using express and feature_flipper. Please make a request to /<user> where <user> is \'mary\', \'john\' or \'admin\'');
 });
 
 
