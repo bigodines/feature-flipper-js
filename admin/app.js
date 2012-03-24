70c4c71b3f2564236cb3051a3d9ba70bcc0c18cd
Matheus Mendes <bigodines@gmail.com>
2012-03-24 19:18:50 -0300

    playing with the next feature: Admin tool to manage features

---
+++ admin/app.js
@@ -0,0 +1,42 @@
+/*
+  Feature flipper admin tool
+
+The aim of this app is to provide a way to manage your features throught feature flipper. Of course you are not required to use this in order to use the library itself!!!
+*/
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'your secret here' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
