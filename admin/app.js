2ada3cb5d15b95bcd829d95a34cb34c097aedfa3
Matheus Mendes <bigodines@gmail.com>
2012-03-24 19:42:30 -0300

    dumb version of the future admin tool. just to test integrating Node+Express+Bootstrap+Jade :D

--- admin/app.js
+++ admin/app.js
@@ -37,6 +37,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.post('/', routes.login);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- admin/routes/index.js
+++ admin/routes/index.js
@@ -1,8 +1,24 @@
-
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('login', { title: 'Feature Flipper - Admin tool' })
+  res.render('login');
+};
+
+exports.login = function(req, res) {
+    var user = req.body.username;
+    var password = req.body.userpass;
+
+    console.log(user,password);
+
+
+    if (user === 'admin' && password === 'featureflipper') {
+        console.log('eeee!!');
+        res.render('index');
+    }
+    else {
+        console.log('try again');
+        res.render('login', {error: 'Invalid Login'});
+    }
 };
