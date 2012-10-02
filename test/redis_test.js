22a5e2502d38c197a02f31884908d5bc35df83ce
Henri Bergius <henri.bergius@iki.fi>
2012-10-02 12:11:08 +0200

    Tabs to spaces

--- test/redis_test.js
+++ test/redis_test.js
@@ -5,31 +5,31 @@ var should = require('should'),
 describe('Redis Storage', function() {
 
     describe('Basic Stuff', function() {
-	it('should be able to take a json and save', function(done) {
-	    var serialized_object = "{ 'foo' : 'bar', 'john' : 'doe' }",
-	        result = storage.set('id', serialized_object, done);
+        it('should be able to take a json and save', function(done) {
+            var serialized_object = "{ 'foo' : 'bar', 'john' : 'doe' }",
+                result = storage.set('id', serialized_object, done);
 
-	    result.should.equal(true);
-	});
-	
-	it('should be able to retrieve', function(done) {
-	    var serialized_object = "{ 'foo' : 'bar', 'john' : 'doe' }";
- 	    storage.get('id', function(err, data) {
-		data.should.eql(serialized_object);
-		done();
-	    });
-	});
+            result.should.equal(true);
+        });
+        
+        it('should be able to retrieve', function(done) {
+            var serialized_object = "{ 'foo' : 'bar', 'john' : 'doe' }";
+            storage.get('id', function(err, data) {
+                data.should.eql(serialized_object);
+                done();
+            });
+        });
 
-	it('should be able to delete', function(done) {
-	    storage.set('foo', 'foo_value');
-	    var result = storage.del('foo')
-	    result.should.equal(true);
-	    storage.get('foo', function(err, data) {
-		should.equal(data, null);
-		done();
-	    });
+        it('should be able to delete', function(done) {
+            storage.set('foo', 'foo_value');
+            var result = storage.del('foo')
+            result.should.equal(true);
+            storage.get('foo', function(err, data) {
+                should.equal(data, null);
+                done();
+            });
 
-	});
+        });
 
     });
 
