e69116b31c564e4ac80d5210675d4909e7f20cad
Matheus Mendes <bigodines@gmail.com>
2015-11-29 13:57:02 -0800

    Creates a memcache storage module
    + Adds support for a memcached storage layer using MemJS
    
    ~
    
    Tests will fail if you don't have memcache running AND redis running. I'm
    working on this next, as well as removing the node_modules/redis dependency.
    Tests will mock everything.

---
+++ storage/ff_memcache.js
@@ -0,0 +1,22 @@
+"use strict";
+(function () {
+    var _memjs = require('memjs');
+    var ff_memcache = function(servers, options) {
+        var mc = _memjs.Client.create(servers, options);
+        return {
+            set: function(key, value, cb) {
+                mc.set(key, value, cb);
+            },
+
+            get: function(key, cb) {
+                mc.get(key, cb);
+            },
+
+            del: function(key, cb) {
+                mc.delete(key, cb);
+            }
+        }
+    }
+
+    module.exports = ff_memcache;
+})();
