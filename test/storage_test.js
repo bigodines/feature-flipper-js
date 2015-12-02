6b941ed71646210138f30d21f92bb484e880832a
Matheus Mendes <bigodines@gmail.com>
2015-12-02 10:03:02 -0800

    Merges all storage tests into a single test file.
    It makes sense to make the same tests running against every storage so that we
    make sure they follow the same contract (no interfaces in javascript..
    remember?)

--- test/storage_test.js
+++ test/storage_test.js
@@ -7,6 +7,20 @@ describe('In-memory storage tests', function () {
     });
 });
 
+describe('Redis storage tests', function () {
+    it('should set/get/delete just like the other storage engines', function(done) {
+        var storage = require('../storage/ff_redis')(6379, 'localhost');
+        run_storage_tests(storage, done);
+    });
+});
+
+describe('Memcache storage tests', function () {
+    it('should set/get/delete just like other storage engines', function (done) {
+        var storage = require('../storage/ff_memcache')(/*servers, options */);
+        run_storage_tests(storage, done);
+    });
+});
+
 function run_storage_tests(storage, done) {
     var serialized_object = "{ 'foo' : 'bar', 'john' : 'doe' }";
     var delete_cb = function() {
